6847bbe62f53196d571ec150b3b9efbd443b4f72
vedant7151 <techkool7306@gmail.com>
2025-11-25 19:25:51 +0530

    updated index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -47,7 +47,7 @@ const syncUserUpdation = inngest.createFunction(
                 id : data.id
             },
             data:{
-                email : data.email_addresses[0]?.email_addres,
+                email : data.email_addresses[0]?.email_address,
                 name : data?.first_name + " " + data?.last_name,
                 image : data?.image_url,
             }
