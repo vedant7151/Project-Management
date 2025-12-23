97e1146a8731cb17a371ec2f981f76976d6e4df0
vedant7151 <techkool7306@gmail.com>
2025-12-23 16:40:36 +0530

    Inngest user functions

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -1,12 +1,11 @@
 // config/prisma.js  (or prisma.js in your uploaded files)
 import 'dotenv/config'
-// import pkg from '@prisma/client'
-// const { PrismaClient } = pkg
-
+import pkg from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { neonConfig } from '@neondatabase/serverless'
 import ws from 'ws'
 
+const { PrismaClient } = pkg
 neonConfig.webSocketConstructor = ws
 
 const connectionString = process.env.DATABASE_URL
